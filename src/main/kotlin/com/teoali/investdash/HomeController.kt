5cf891174a4c961cacbe118761bd51672ebbee96
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2025-03-22 16:30:16 -0300

    wip

--- src/main/kotlin/com/teoali/investdash/HomeController.kt
+++ src/main/kotlin/com/teoali/investdash/HomeController.kt
@@ -1,17 +1,19 @@
 package com.teoali.investdash
 
 import org.jsoup.Jsoup
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HomeController {
 
     @GetMapping("/")
-    fun home(): String {
+    fun home(model: Model): String {
         val dollar = getDollar()
+        model.addAttribute("result", dollar)
         //val bitcoin = getBitcoin()
-        return dollar
+        return "index"
     }
 
     fun getDollar(): String {
